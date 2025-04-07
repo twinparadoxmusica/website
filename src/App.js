e178a8ec18d5b22188c03fe96880cc4a0d1a0062
Twin Paradox <twinparadoxmusica@gmail.com>
2025-04-07 14:16:22 +0900

    formatting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
-import "./App.css";
-import { runAnimation } from "./utils/animation";
-import logo from "./assets/twin-paradox-typography.png";
+import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import { runAnimation } from './utils/animation';
+import logo from './assets/twin-paradox-typography.png';
 
 function App() {
   useEffect(() => {
@@ -20,13 +21,13 @@ function App() {
         <p>
           Twin Paradox, an Argentinian electronic music duo based in Geneva, was
           formed in 2023 by Mirko Hrubik and Ezequiel Cappellano. They create
-          hypnotic and energetic <b>Techno</b> with <b>Rock/Metal</b>{" "}
+          hypnotic and energetic <b>Techno</b> with <b>Rock/Metal</b>{' '}
           influences, using analog and digital hardware with instruments like
-          guitar, voice and synthesizers, performing both <b>Live Sets</b> and{" "}
+          guitar, voice and synthesizers, performing both <b>Live Sets</b> and{' '}
           <b>DJ formats</b>.
         </p>
         <p>
-          Read full bio at{" "}
+          Read full bio at{' '}
           <a href="https://ra.co/dj/twinparadox/biography">Resident Advisor</a>
         </p>
       </section>
@@ -39,9 +40,9 @@ function App() {
             height="315"
             src="https://www.youtube.com/embed/GEH6M5wipNk?si=KqaSQNbyuCt7Mbnf"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
+            referrerPolicy="strict-origin-when-cross-origin"
             allowfullscreen
           ></iframe>
         </div>
@@ -51,9 +52,9 @@ function App() {
             height="315"
             src="https://www.youtube.com/embed/P361vVvKCXg?si=5we2CFduNi58eQ8L"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
+            referrerPolicy="strict-origin-when-cross-origin"
             allowfullscreen
           ></iframe>
         </div>
--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,38 +1,38 @@
 export function runAnimation() {
-  const canvas = document.getElementById("galaxyCanvas");
-  const ctx = canvas.getContext("2d");
+  const canvas = document.getElementById('galaxyCanvas');
+  const ctx = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
   const stars = [];
   for (let i = 0; i < 150; i++) {
     stars.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        radius: Math.random() * 2,
-        speed: Math.random() * 0.5 + 0.2
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      radius: Math.random() * 2,
+      speed: Math.random() * 0.5 + 0.2,
     });
   }
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-    
-    stars.forEach(star => {
-        ctx.beginPath();
-        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-        ctx.fill();
-          star.y += star.speed;
-          if (star.y > canvas.height) star.y = 0;
-      });
-      
-      requestAnimationFrame(animate);
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+
+    stars.forEach((star) => {
+      ctx.beginPath();
+      ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+      ctx.fill();
+      star.y += star.speed;
+      if (star.y > canvas.height) star.y = 0;
+    });
+
+    requestAnimationFrame(animate);
   }
 
   animate();
 
-  window.addEventListener("resize", () => {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+  window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   });
 }
