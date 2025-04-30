6d2c14b6dc449f282305fd3cd54825fa09dac67b
Twin Paradox <twinparadoxmusica@gmail.com>
2025-04-30 11:32:32 +0200

    refactor on sections and added meta tags

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact" className="section">
+    <h2>Contact</h2>
+    <p>Feel free to reach out to the following email:</p>
+    <p>
+      <a data-name="mail-text" href="mailto:twinparadoxmusica@gmail.com">
+        twinparadoxmusica@gmail.com
+      </a>
+    </p>
+  </section>
+);
+
+export default Contact;
