41a6f996ac5a6724f1f4e5b09b91175a68509632
Twin Paradox <twinparadoxmusica@gmail.com>
2025-04-30 12:06:15 +0200

    added presskit links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,26 @@ import React from 'react';
 const Contact = () => (
   <section id="contact" className="section">
     <h2>Contact</h2>
-    <p>Feel free to reach out to the following email:</p>
+    <p>Feel free to reach out at:</p>
     <p>
       <a data-name="mail-text" href="mailto:twinparadoxmusica@gmail.com">
         twinparadoxmusica@gmail.com
+      </a>
+    </p>
+    <p>
+      <a
+        data-name="presskit-english"
+        href="https://www.twinparadox.music/assets/TwinParadoxPressKit-EN.pdf"
+      >
+        PressKit (English)
+      </a>
+    </p>
+    <p>
+      <a
+        data-name="presskit-french"
+        href="https://www.twinparadox.music/assets/TwinParadoxPressKit-FR.pdf"
+      >
+        Press Kit (Français)
       </a>
     </p>
   </section>
