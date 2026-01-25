706bf4df0c0be51a31a666232e045aac027e925a
Twin Paradox <twinparadoxmusica@gmail.com>
2026-01-25 14:23:28 +0100

    updated meta pixel

--- app/layout.js
+++ app/layout.js
@@ -215,14 +215,15 @@ const RootLayout = ({ children }) => {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];t=b.createElement(e);t.async=!0;
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1840016840116319');
+              fbq('init', '1023035406591668');
               fbq('track', 'PageView');
             `,
           }}
@@ -232,7 +233,7 @@ const RootLayout = ({ children }) => {
             height="1"
             width="1"
             style={{ display: 'none', visibility: 'hidden' }}
-            src="https://www.facebook.com/tr?id=1840016840116319&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1023035406591668&ev=PageView&noscript=1"
           />
         </noscript>
         {/* Google Tag Manager noscript */}
